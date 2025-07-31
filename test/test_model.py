48623bd3a2ba5a9c76f45a7be1cdb23a25fd729b
Pankaj Soni <pycsrbypankaj@gmail.com>
2025-07-31 21:36:20 +0530

    .

--- test/test_model.py
+++ test/test_model.py
@@ -7,7 +7,7 @@ def test_model_accuracy():
     df = pd.read_csv(iris)
     X, y = df.iloc[:,:-1],df.iloc[:,-1]    
 
-    model = joblib.load("model/model.pkl")
+    model = joblib.load("models/iris_model.pkl")
     score = model.score(X, y)
     
     assert score > 0.9, f"Accuracy too low: {score}"
