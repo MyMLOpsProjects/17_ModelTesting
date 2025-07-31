c671d5e4dcb2a797143ee52e94b69bd57e46baad
Pankaj Soni <pycsrbypankaj@gmail.com>
2025-07-31 20:58:11 +0530

    Added all required files

---
+++ model/train.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+iris = "./data/iris.csv"
+df = pd.read_csv(iris)
+X, y = df.iloc[:,:-1],df.iloc[:,-1]
+
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, 'model/model.pkl')
